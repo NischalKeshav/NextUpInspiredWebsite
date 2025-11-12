97042d755c976d822b480503b8c94c9e621b4cd6
Nischal Keshav <nischalkeshav.official@gmail.com>
2025-11-11 18:29:18 -0600

    nextup

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,14 @@
+<script>
+	import {Navlinks, Navlist } from '$lib/index.js'
+</script>
+
+<nav>
+<ul>
+{#each Navlist as ni, index}
+
+	<a href="google.com" >{ni}</a>
+	
+{/each}
+</ul>
+</nav>
+
---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+// place files you want to import through the `$lib` alias in this folder.
+export const Navlist = ['Home', 'Impacts', 'Sponsors', 'Contact'];
+export const Navlinks =  ['/Home', '/Impacts', '/Sponsors', '/Contact'];
+
