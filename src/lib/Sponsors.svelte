aa6c6a33c088e8e11627c4bb2fba3099bc11bd24
Nischal Keshav <nischalkeshav.official@gmail.com>
2025-11-23 18:09:08 -0600

    s update

---
+++ src/lib/Sponsors.svelte
@@ -0,0 +1,19 @@
+<div class="sponsors">
+    <h1>Our Sponsors</h1>
+</div>
+
+<style>
+    .sponsors {
+        background-color: #F8F7EE;
+        color: #1A345B;
+        height: 100vh;
+        width: 100%;
+    }
+    h1 {
+        font-size: 4rem;
+        font-weight: 600;
+        text-align: center;
+        margin-top: 2rem;
+        color: #1A345B;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,11 @@
 	import Navbar from '$lib/Navbar.svelte'
 	import Hero from '$lib/Hero.svelte'
 	import Impact from '$lib/Impact.svelte'
+	import Sponsors from '$lib/Sponsors.svelte'
 </script>
 
 
 <Navbar></Navbar>
 <Hero></Hero>
 <Impact></Impact>
+<Sponsors></Sponsors>
